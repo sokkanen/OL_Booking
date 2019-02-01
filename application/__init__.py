676ead385b12fc3c6fa604c3367c37e2caff5a4b
sokkanen <joel.sokkanen@helsinki.fi>
2019-02-01 14:41:15 +0200

    Herokun tietokanta-asetukset

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///olbooking.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///olbooking.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -21,6 +27,8 @@ from application.customer import models
 from application.account import models
 from application.account import views
 
+
+# Kirjautuminen.
 from application.account.models import Account
 from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
@@ -35,5 +43,6 @@ login_manager.login_message = "Please login to use this functionality"
 @login_manager.user_loader
 def load_account(account_id):
     return Account.query.get(account_id)
-  
+
+# Luo taulut
 db.create_all()
