a5a3fcaa9dd60fbcb3ebf4c0d03d180aa89df5dc
sokkanen <joel.sokkanen@helsinki.fi>
2019-02-04 00:56:34 +0200

    Korjauksia Herokuun

--- application/booking/models.py
+++ application/booking/models.py
@@ -27,7 +27,7 @@ class Booking(Base):
                     " FROM Booking LEFT JOIN Worker ON Worker.id = Booking.worker_id"
                     " LEFT JOIN Service ON Booking.service_id = Service.id"
                     " WHERE Booking.confirmed = 1"
-                    " GROUP BY Booking.requested_date")
+                    " GROUP BY Booking.requested_date, Worker.name, Service.duration_hrs, Service.duration_mins")
         res = db.engine.execute(stmt)
 
         lst = []
